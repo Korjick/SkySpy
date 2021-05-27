d9f1283324a0e02da92ed7f7aa9dc371ceab084f
Korjick <zzgriim@gmail.com>
2021-05-27 13:28:33 +0300

    meta

--- Assets/Scripts/Player/Bullet.cs
+++ Assets/Scripts/Player/Bullet.cs
@@ -1,32 +1,34 @@
 using System.Collections;
-using System.Collections.Generic;
 using Assets.Scripts.Interfaces;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class Bullet : MonoBehaviour
+namespace Assets.Scripts.Player
 {
-    public int bulletLifeTime = 10;
-    public int damage = 10;
-    
-    void Awake()
+    [RequireComponent(typeof(Rigidbody))]
+    public class Bullet : MonoBehaviour
     {
-        StartCoroutine(DestroyBullet());
-    }
+        public int bulletLifeTime = 10;
+        public int damage = 10;
+    
+        void Awake()
+        {
+            StartCoroutine(DestroyBullet());
+        }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(collision.collider.name);
+        void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log(collision.collider.name);
         
-        if (collision.collider.GetComponent<IDamagable>() != null && !collision.collider.CompareTag(transform.tag))
-            collision.collider.GetComponent<IDamagable>().GetDamage(damage);
+            if (collision.collider.GetComponent<IDamagable>() != null && !collision.collider.CompareTag(transform.tag))
+                collision.collider.GetComponent<IDamagable>().GetDamage(damage);
         
-        Destroy(gameObject);
-    }
+            Destroy(gameObject);
+        }
     
-    private IEnumerator DestroyBullet()
-    {
-        yield return new WaitForSeconds(bulletLifeTime);
-        Destroy(gameObject);
+        private IEnumerator DestroyBullet()
+        {
+            yield return new WaitForSeconds(bulletLifeTime);
+            Destroy(gameObject);
+        }
     }
 }
