84ade59f329242a03b6e29530adbd207810a063a
Korjick <zzgriim@gmail.com>
2021-06-09 12:42:18 +0300

    map remap

--- Assets/Scripts/Game/MenuController.cs
+++ Assets/Scripts/Game/MenuController.cs
@@ -3,12 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     public static MenuController menuController;
 
     public Canvas menu, map;
+    public Button demo;
     
     private void Awake()
     {
@@ -24,5 +26,13 @@ public class MenuController : MonoBehaviour
     public void LoadLevel(int id)
     {
         SceneManager.LoadScene(id);
+    }
+
+    public void SecretLevel()
+    {
+        if (PlayerPrefs.HasKey("Demo"))
+        {
+            demo.onClick.AddListener(() => SceneManager.LoadScene(2));
+        }
     }
 }
